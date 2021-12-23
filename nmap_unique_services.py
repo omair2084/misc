6a87748a524ba4e5c262c8c1b39ef28ea9f6d0e8
omair2084 <omairmo@outlook.com>
2021-12-23 19:12:56 +0530

    Update nmap_unique_services.py

--- nmap_unique_services.py
+++ nmap_unique_services.py
@@ -1,17 +1,24 @@
+#Python3
 #Print unique services found on the network
 #using the xml output from nmap
 #nmaprun > host > ports > port > service
 
-
 import xml.etree.ElementTree
-e = xml.etree.ElementTree.parse('basic_scans.xml').getroot()
+import sys
+
+if len(sys.argv) != 2:
+    print("[-] Supply nmap .xml outputfile")
+    print("Help: %s scanOutput.xml" % sys.argv[0])
+    sys.exit(-1)
+
+e = xml.etree.ElementTree.parse(sys.argv[1]).getroot()
 fe = open("services_only.txt","w")
 for elem in e.findall('host/ports/port/service'):
     z = str(elem.get('product'))+"\n"
-	fe.write(z)
+    fe.write(z)
 
 f = open("services_only.txt", "r")
 lines = set(f.readlines())
-print ''.join(lines)
+print (''.join(lines))
 
 
