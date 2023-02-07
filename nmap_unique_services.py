3da5e7164c5dbecd392db252e371dbb227fd6288
omair2084 <omairmo@outlook.com>
2023-02-08 04:52:04 +0530

    needed to close()

--- nmap_unique_services.py
+++ nmap_unique_services.py
@@ -6,18 +6,19 @@
 import xml.etree.ElementTree
 import sys
 
-if len(sys.argv) != 2:
-    print("[-] Supply nmap .xml outputfile")
-    print("Help: %s scanOutput.xml" % sys.argv[0])
+if len(sys.argv) < 3:
+    print("[-] Supply nmap.xml and output file")
+    print("Help: python3 %s scanOutput.xml storeOutput.txt" % sys.argv[0])
     sys.exit(-1)
 
 e = xml.etree.ElementTree.parse(sys.argv[1]).getroot()
-fe = open("services_only.txt","w")
+fe = open(sys.argv[2],"w")
 for elem in e.findall('host/ports/port/service'):
     z = str(elem.get('product'))+"\n"
     fe.write(z)
+fe.close()
 
-f = open("services_only.txt", "r")
+f = open(sys.argv[2], "r")
 lines = set(f.readlines())
 print (''.join(lines))
 
